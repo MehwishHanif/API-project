44273e1e269b161812689eb9cb352ca6ab64e4f8
MehwishHanif <mehwish.hanif90@gmail.com>
2024-12-11 14:36:49 -0800

    Add error-handling middleware for common errors and debugging

---
+++ backend/routes/users.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.send('User list');
+// });
+
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params;
+//   res.send(`User details for ID: ${id}`);
+// });
+
+// module.exports = router;
